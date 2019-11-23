81c5ce6dbde66b199a5e67f5f7f6b1145ec57b8a
Marcin Chwedczuk <marcin@sumologic.com>
2019-11-23 11:34:50 +0100

    ex 3.1-3.2

--- src/pl/marcinchwedczuk/fpinscala/chp3/List.scala
+++ src/pl/marcinchwedczuk/fpinscala/chp3/List.scala
@@ -1,5 +1,7 @@
 package pl.marcinchwedczuk.fpinscala.chp3
 
+import pl.marcinchwedczuk.fpinscala.chp3.List.tail
+
 sealed trait List[+A]
 
 case object Nil extends List[Nothing]
@@ -9,5 +11,19 @@ object List {
   def apply[A](as: A*): List[A] = {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+  }
+
+  def tail[A](as: List[A]): List[A] = {
+    as match {
+      case Cons(_, tail) => tail
+      case Nil => throw new RuntimeException("Cannot get tail of empty list.")
+    }
+  }
+}
+
+object Program {
+  def main(args: Array[String]): Unit = {
+    val list = List(1,2,3,4)
+    println(tail(list))
   }
 }
